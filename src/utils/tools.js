66d4a9d81bcb7e890cbc062926849ccdeeac2944
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2023-12-21 18:56:30 +0800

    新增deepClone方法

---
+++ src/utils/tools.js
@@ -0,0 +1,16 @@
+// 深度克隆
+export const deepClone = (obj) => {
+    if (obj === null) return null;
+    if (typeof obj !== "object") return obj;
+    if (obj.constructor === Date) return new Date(obj);
+    if (obj.constructor === RegExp) return new RegExp(obj);
+    let newObj = new obj.constructor(); //保持继承链
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            //不遍历其原型链上的属性
+            let val = obj[key];
+            newObj[key] = typeof val === "object" ? deepClone(val) : val; // 使用arguments.callee解除与函数名的耦合
+        }
+    }
+    return newObj;
+};
